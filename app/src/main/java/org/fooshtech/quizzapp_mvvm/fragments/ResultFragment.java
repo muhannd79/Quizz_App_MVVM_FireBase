723599528e4d9dbace349d9d75a66882781301f7
muhannd79 <muhannddh@gmail.com>
2020-03-09 13:19:50 -0500

    Initial commit

---
+++ app/src/main/java/org/fooshtech/quizzapp_mvvm/fragments/ResultFragment.java
@@ -0,0 +1,29 @@
+package org.fooshtech.quizzapp_mvvm.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.fooshtech.quizzapp_mvvm.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ResultFragment extends Fragment {
+
+    public ResultFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+}
